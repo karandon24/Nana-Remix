5aa19d47e631f8339cb76d8e1e36ec32fa4a71f1
Poki <aman@virtual.mv>
2020-06-23 18:28:44 +0500

    Init

---
+++ nana/modules/termux.py
@@ -0,0 +1,15 @@
+
+
+from pyrogram import Filters
+from nana import Command, app
+
+__MODULE__ = "Termux"
+__HELP__ = """
+Paste Media Documents on Telegra.ph
+
+──「 **Telegra.ph** 」──
+-> `telegraph (reply to a media)`
+Reply to Media as args to upload it to telegraph.
+- Supported Media Types (.jpg, .jpeg, .png, .gif, .mp4)
+
+"""
