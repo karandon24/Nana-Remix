75319e6bb33d687ffe8c9a5ee66935be915e4dd3
Poki <aman@virtual.mv>
2020-06-23 20:21:17 +0500

    Added bstat for termux

--- nana/modules/termux.py
+++ nana/modules/termux.py
@@ -1,15 +1,31 @@
 
 
 from pyrogram import Filters
-from nana import Command, app
+import json
+from asyncio import sleep
+import subprocess
+
+from nana import Command, app, TERMUX_USER
 
 __MODULE__ = "Termux"
 __HELP__ = """
-Paste Media Documents on Telegra.ph
+For Termux API (You may need to host your Userbot on Termux):
+And install Termux API from Playstore.
 
-──「 **Telegra.ph** 」──
--> `telegraph (reply to a media)`
-Reply to Media as args to upload it to telegraph.
-- Supported Media Types (.jpg, .jpeg, .png, .gif, .mp4)
+──「 **Battery Status** 」──
+-> `bstats`
+get yout phone's Battery Status.
 
 """
+
+@app.on_message(Filters.me & Filters.command(["bstats"], Command))
+async def bstat(message):
+    if TERMUX_USER:
+        termux_command = subprocess.Popen("termux-battery-status", shell=True, stdout=subprocess.PIPE)
+        my_bytes_value = termux_command.stdout.read()
+        my_json = my_bytes_value.decode('utf8').replace("'", '').replace('"', '').replace("{", '').replace("}", '').replace(",", '').replace(" ", '')
+        await message.edit(f"<b>Battery Status:</b>{my_json}")
+    else:
+        await message.edit("This command is only for Termux users!")
+        await sleep(2.0)
+        await message.delete()
